0c4fcfdfb1793608abed3f7ca41caaedc782e83f
Gabe Thompson <gabe.th@gmail.com>
2024-01-18 22:09:01 -0800

    Changed a few things in the test kit - namely noted that add_event_detect function just doesn't work reliably

--- keypadtest.py
+++ keypadtest.py
@@ -1,14 +1,14 @@
 import RPi.GPIO as GPIO
 import time
 
-R1 = 3
-R2 = 2
-R3 = 1
-R4 = 0
+R1 = 26
+R2 = 19
+R3 = 13
+R4 = 6
 
-C1 = 6
-C2 = 5
-C3 = 4
+C1 = 21
+C2 = 20
+C3 = 16
 #C4 = 21 #Only 3 columns
 
 GPIO.setwarnings(False)
@@ -28,17 +28,21 @@ def keypad_column_func(column):
     print(f'Column {column} was pressed')
 
 time.sleep(1)
-GPIO.add_event_detect(C1, GPIO.RISING, bouncetime=200, callback=keypad_column_func)
-GPIO.add_event_detect(C2, GPIO.RISING, bouncetime=200, callback=keypad_column_func)
-GPIO.add_event_detect(C3, GPIO.RISING, bouncetime=200, callback=keypad_column_func)
+#The add_event_detect function just doesn't work well without some hardware debouncing
+GPIO.add_event_detect(C1, GPIO.RISING, bouncetime=2000, callback=keypad_column_func)
+GPIO.add_event_detect(C2, GPIO.RISING, bouncetime=2000, callback=keypad_column_func)
+GPIO.add_event_detect(C3, GPIO.RISING, bouncetime=2000, callback=keypad_column_func)
 
 def readLine(line, characters):
     GPIO.output(line, GPIO.HIGH)
     if(GPIO.input(C1) == 1):
+        pass
         print(characters[0])
     if(GPIO.input(C2) == 1):
+        pass
         print(characters[1])
     if(GPIO.input(C3) == 1):
+        pass
         print(characters[2])
     # if(GPIO.input(C4) == 1): #Only have 3 columns
     #     print(characters[3])
